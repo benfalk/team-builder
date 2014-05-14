802898110ea73250ec3323866ddb2af3897792cb
Brad Potterbaum <bpotterbaum@gmail.com>
2014-05-14 16:40:41 -0700

    building out pages

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   get 'team/' => 'pages#team'
+  get 'comrades/' => 'pages#comrades'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
