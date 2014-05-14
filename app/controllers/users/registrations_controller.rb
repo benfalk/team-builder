fe3344ad66a5e454dd789a5abfbbb8e67e742bff
Benjamin Falk <benjamin.falk@yahoo.com>
2014-05-14 11:22:40 -0700

    Hooking up most of the sign up form

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,42 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
+  before_filter :set_summoner, only:[:create]
+
   # GET /users/sign_up
   def new
     @summoner = Summoner.find(params[:summoner])
-    build_resource({summoner_id: @summoner.id})
+    build_resource({summoner: @summoner})
   end
+
+  private
+
+  def sign_up_params
+    params.require(
+      :user
+    ).permit(
+      :first_name,
+      :last_name,
+      :location_city,
+      :location_state,
+      :short_bio,
+      :password,
+      :password_confirmation,
+      :summoner_id,
+      :email,
+      :play_style,
+      :gaming_times,
+      :avatar,
+      { :favorite_role_ids => [] }
+    ).tap do |p|
+      if params[:favorite_champs]
+        p[:favorite_champion_ids] = Champion.where(key: params[:favorite_champs]).pluck(:id)
+      end
+    end
+  end
+
+  def set_summoner
+    @summoner = Summoner.find(params[:user][:summoner_id])
+  end
+
 
 end
