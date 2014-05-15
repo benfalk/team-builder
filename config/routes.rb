92fda4dbff27b1ee458e171e03e1f48b8f5d129e
Brad Potterbaum <bpotterbaum@gmail.com>
2014-05-15 12:26:27 -0700

    adding in toastr and playing with playbook

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get 'team/' => 'pages#team'
   get 'comrades/' => 'pages#comrades'
   get 'join/' => 'pages#join'
+  get 'playbook/' => 'pages#playbook'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
