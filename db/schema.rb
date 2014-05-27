ffa6d7f7dcfd5d70ac901b1b34a988ede9519897
Brad Potterbaum <bpotterbaum@gmail.com>
2014-05-27 10:22:15 -0700

    cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140525181751) do
+ActiveRecord::Schema.define(version: 20140527155329) do
 
   create_table "champion_preferences", force: true do |t|
     t.integer  "user_id"
