9ab2aef9bcf377083bbcf881632eb52b66e06911
Brad Potterbaum <bpotterbaum@gmail.com>
2014-06-24 22:38:48 -0700

    adding skils

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -10,7 +10,28 @@ class Skill < ActiveRecord::Base
       'Mid Lane',
       'Jungle',
       'AD carry',
-      'Support'
+      'Support',
+      'Map Awareness',
+      'Peeling',
+      'Ganking',
+      'juggernaut',
+      'Mind Reader',
+      'Long Shot',
+      'Farm, farm, farm',
+      'Great Attitude',
+      'Ultimate disrespect',
+      'Made me laugh',
+      'encourager',
+      'Good Advice',
+      'tasteful humor',
+      'Want on my team',
+      'Good communicator',
+      'Lane Freezing',
+      'Proxying',
+      'Map awareness',
+      'Streamer',
+      'Lane Synergy'
+  
   ]
 
 end
