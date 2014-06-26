cba19fdad8b93338626a3889c5960c53104955df
Benjamin Falk <benjamin.falk@yahoo.com>
2014-06-26 15:29:22 -0700

    In goes 'percent_of_favorite'

--- app/models/champion_preference.rb
+++ app/models/champion_preference.rb
@@ -10,4 +10,14 @@ class ChampionPreference < ActiveRecord::Base
     update(played_count: GameStats.where(summoner_id: user.summoner_id, played_champion_id: champion_id).count) 
   end
 
+  def percent_of_favorite
+    total = user.favorite_champion_preferences.sum(:played_count)
+
+    if total == 0
+      0
+    else
+      ( played_count / total.to_f ).round(4) * 100
+    end
+  end
+
 end
